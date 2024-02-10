380168e06d7b0eb46ee27efafc9ce0107b4e14ea
Nagez <81879748+Nagez@users.noreply.github.com>
2024-02-10 15:27:21 +0200

    handle errors

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -167,6 +167,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
+    //console.log("fetchById" + invoice); // Invoice is an empty array []
 
     return invoice[0];
   } catch (error) {
